d3ce75b778f1faab86eb67f22144199aac30bfa5
Amar AA <amar.ashfaq95@gmail.com>
2025-12-03 23:41:49 +0000

    Applied validation (data annotations) to the BookCreateDto and BookUpdateDto. In addtion, a custom data annotation attribute was created.

--- DTOs/BookUpdateDto.cs
+++ DTOs/BookUpdateDto.cs
@@ -1,12 +1,31 @@
-﻿namespace BookLibraryAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryAPI.DTOs
 {
     public class BookUpdateDto
     {
+        [Required(ErrorMessage = "Book title is required")]
+        [StringLength(50, ErrorMessage = "Book title cannot be longer than 50 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Book description is required")]
+        [StringLength(500, ErrorMessage = "Book description cannot be longer than 500 characters")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Book author is required")]
+        [StringLength(50, ErrorMessage = "Book author cannot be longer than 50 characters")]
         public string Author { get; set; }
+
+        [Required(ErrorMessage = "Book genre is required")]
+        [StringLength(50, ErrorMessage = "Book genre cannot be longer than 50 characters")]
         public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Published year is required")]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Year must be a 4-digit number.")]
+        [YearRange(1900)]
         public int PublishedYear { get; set; }
+
+        [Required]
         public bool IsAvailable { get; set; }
     }
 }
---
+++ YearRangeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryAPI
+{
+    public class YearRangeAttribute : ValidationAttribute
+    {
+        private readonly int _minYear;
+
+        public YearRangeAttribute(int minYear)
+        {
+            _minYear = minYear;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is int year)
+            {
+                int maxYear = DateTime.Now.Year;
+
+                if (year < _minYear || year > maxYear)
+                {
+                    return new ValidationResult(
+                        $"Year must be between {_minYear} and {maxYear}.");
+                }
+
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Invalid year format.");
+        }
+    }
+}
