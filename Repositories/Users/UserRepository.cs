47a7a57592f884161693a8c5bf0480f367ec8945
Amar AA <amar.ashfaq95@gmail.com>
2026-04-23 14:38:19 +0100

    Separated responsibilities between auth and user registration. Refactored parts between the service and repository layers.

--- Repositories/Users/UserRepository.cs
+++ Repositories/Users/UserRepository.cs
@@ -1,18 +1,15 @@
 ﻿using BookLibraryAPI.Data;
 using BookLibraryAPI.Entities;
-using BookLibraryAPI.Services.Auth;
 
 namespace BookLibraryAPI.Repositories.Users
 {
     public class UserRepository : IUserRepository
     {
         private readonly AppDbContext _context;
-        private readonly IPasswordService _passwordService;
 
-        public UserRepository(AppDbContext context, IPasswordService passwordService)
+        public UserRepository(AppDbContext context)
         {
             _context = context;
-            _passwordService = passwordService;
         }
         public List<User> GetUsers()
         {
@@ -46,11 +43,6 @@ namespace BookLibraryAPI.Repositories.Users
 
         public User CreateUser(User user)
         {
-            var hash = _passwordService.HashPassword(user.PasswordHash, out var salt);
-
-            user.PasswordHash = hash;
-            user.PasswordSalt = Convert.ToHexString(salt);
-
             _context.Users.Add(user);
             _context.SaveChanges();
 
